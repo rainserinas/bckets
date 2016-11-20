ebcaa47f5be70d0727d810757842b690ac5a515e
mauro ricardo marfil <mauro ricardo marfil>
2016-11-20 09:06:17 +0800

    Added Header and modal login

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
+$(function() {
+    attachListenerHeaderNavbar();
+    attachListenerHamburgerMenu();
+});
 
-$('.datepicker').datepicker();
+
+function attachListenerHeaderNavbar() {
+	function navSlide() {
+       var scroll_top = $(window).scrollTop();
+       var url = window.location.href;
+        if (scroll_top >= 150) { 
+       		$("#header").addClass('sticky');
+        }else{
+            $("#header").removeClass('sticky');
+        } 
+   }
+   $(window).scroll(navSlide);
+}
+function attachListenerHamburgerMenu() {
+    //Open Hamburger Menu
+    $( ".hamburger i" ).click(function() {
+        $('#mySidenav').css('width','100%');
+        $('body').css('background-color','rgba(0,0,0,0.4)');
+    });
+    //Close Hamburger Menu
+    $( ".closebtn,.mobile-login" ).click(function() {
+        $('#mySidenav').css('width','0');
+        $('body').css('background-color','#fff');
+    });
+}
